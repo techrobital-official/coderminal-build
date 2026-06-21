b2bf2ad4abf346b5769e15ebe7179a17de3749fa
techrobital-official <techrobital@gmail.com>
2026-06-21 11:24:09 +0530

    Add Electron Components

---
+++ IDE/main.js
@@ -0,0 +1,9 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1000, height:700
+    });
+    win.loadFile('index.html')
+}
+app.whenReady().then(createWindow);
